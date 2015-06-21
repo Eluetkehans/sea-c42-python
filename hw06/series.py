58f2b1513d682cd7046fd0631c7780899c9d9050
Erik Luetkehans <eluetekhans@gmail.com>
2015-06-20 17:43:43 -0700

    Finish fibonacci function

--- hw06/series.py
+++ hw06/series.py
@@ -1,9 +1,16 @@
 def fibonacci(n=0):
     """Returns the the value of the nth number in the fibonacci sequence"""
+    first_num = 0
+    second_num = 1
     nth = 0
-    for i in range(n):
-        if(nth == 0):
-            nth += 1
-        else:
-            nth += nth
+    if(n <= 1):
+        return first_num
+    else:
+        for i in range(n - 1):
+            nth = first_num + second_num
+            first_num = second_num
+            second_num = nth
     return nth
+
+nth = fibonacci(int(input("number: ")))
+print(nth)
