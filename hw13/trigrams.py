be4a98e5baefa5ec6a6539f0a7ab6e385ffa5010
Erik Luetkehans <eluetekhans@gmail.com>
2015-07-01 18:07:29 -0700

    Actually create the trigram dictionary filling function

--- hw13/trigrams.py
+++ hw13/trigrams.py
@@ -1,2 +1,24 @@
 # Import random
 import random
+
+# Create empty dictionary for our trigrams
+tridic = {}
+
+# Get our text file
+f = file.open("sherlock-small.txt", "r")
+
+# Store the text in a variable
+text = f.getlines()
+
+# Split the text into individual words
+text.split()
+
+
+# Create dictionary of trigrams
+def filldic(text):
+    """Fills the trigram dictionary with trigrams"""
+    for word in range(len(text) - 2):
+        if(tridic[word, word + 1] in tridic):
+            tridic[(word, word + 1)].append(word + 3)
+        else:
+            tridic[(word, word + 1)] = [word + 3]
