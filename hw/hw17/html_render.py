c60dc41c9935c926512257e2de817660c874c486
Erik Luetkehans <eluetekhans@gmail.com>
2015-07-08 16:04:27 -0700

    create the render function for element

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -10,12 +10,16 @@ Python class example.
 # Fill it all in here.
 class Element(object):
 
-    def __init__(self, name, content):
+    def __init__(self, name="", content=""):
         self.name = name
         self.content = content
 
     def append(self, new_text):
         self.content += new_text
 
-    def render(self, file):
-        file.write(self.content)
+    def render(self, file_name, ind=""):
+        indent = ind
+        indent += self.content
+        write_out = open(file_name, "w")
+        write_out.write(indent)
+        write_out.close()
